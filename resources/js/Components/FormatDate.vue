43df7a086186f2b9d258a35d378efff44f6f9980
griffith-xx <chacritpoonpipit@gmail.com>
2025-06-05 10:04:21 +0700

    commit

---
+++ resources/js/Components/FormatDate.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    currentDate: {
+        type: Date,
+        required: true,
+    },
+});
+const formatDate = (date) => {
+    return date.toLocaleDateString("th-TH", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        weekday: "long",
+    });
+};
+</script>
+
+<template>
+    {{ formatDate(currentDate) }}
+</template>
