88e19459855d39b5f19887880069d8f1607eccc9
griffith-xx <chacritpoonpipit@gmail.com>
2025-06-05 10:06:37 +0700

    favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,6 +16,13 @@
     @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
+
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Devwatch" />
+    <link rel="manifest" href="/site.webmanifest" />
 </head>
 
 <body class="font-sans antialiased">
