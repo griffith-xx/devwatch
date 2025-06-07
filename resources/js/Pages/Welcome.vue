e0426f1c9ef1e37d74531e670697978393e5199f
困住你的不是别人 <116521229+griffith-xx@users.noreply.github.com>
2025-06-07 07:39:32 +0700

    commit

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,25 +1,7 @@
 <script setup>
 import { Head } from "@inertiajs/vue3";
-import { ref, onMounted } from "vue";
-import FormatDate from "@/Components/FormatDate.vue";
-import FormatTime from "@/Components/FormatTime.vue";
-const currentDate = ref(new Date());
-onMounted(() => {
-    const interval = setInterval(() => {
-        currentDate.value = new Date();
-    }, 1000);
-    return () => {
-        clearInterval(interval);
-    };
-});
 </script>
 
 <template>
     <Head title="Engineering Service Center" />
-    <p>
-        <FormatDate :current-date="currentDate" />
-    </p>
-    <p>
-        <FormatTime :current-date="currentDate" />
-    </p>
 </template>
